09c27d2d0da7a7f03a3afb98d88fd6b8fad249ba
Brandon Humphry <brandonhumphry@gmail.com>
2021-01-22 23:55:40 -0800

    working with react-hook-form for imput fields in a users message

--- src/SendMail.js
+++ src/SendMail.js
@@ -2,20 +2,43 @@ import React from "react";
 import "./SendMail.css";
 import CloseIcon from "@material-ui/icons/Close";
 import { Button } from "@material-ui/core";
+import { useForm } from "react-hook-form";
 
 function SendMail() {
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
   return (
     <div className="sendMail">
       <div className="sendMail__header">
         <h3>New Message</h3>
         <CloseIcon className="sendMail__close" />
       </div>
-      <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="to"
+          placeholder="To"
+          type="text"
+          ref={register({ required: true })}
+        />
+        <input
+          name="subject"
+          placeholder="Subject"
+          type="text"
+          ref={register({ required: true })}
+        />
+        <input
+          name="message"
+          placeholder="Click to begin typing"
+          type="text"
+          className="sendMail__message"
+          ref={register({ required: true })}
+        />
         <div className="sendMail__options">
-          <Button>Send</Button>
+          <Button className="sendMail__send">Send</Button>
         </div>
       </form>
     </div>
