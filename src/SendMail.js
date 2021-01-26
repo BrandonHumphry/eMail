dd24cb86e7e313fbcdcb5c5e93442f71917dad4e
Brandon Humphry <brandonhumphry@gmail.com>
2021-01-25 22:57:33 -0800

    message isnt being sent as object, trying to find out why

--- src/SendMail.js
+++ src/SendMail.js
@@ -24,12 +24,16 @@ function SendMail() {
           type="text"
           ref={register({ required: true })}
         />
+        {errors.to && <p className="sendMail__error">"To" is required</p>}
         <input
           name="subject"
           placeholder="Subject"
           type="text"
           ref={register({ required: true })}
         />
+        {errors.subject && (
+          <p className="sendMail__error">"Subject" is required</p>
+        )}
         <input
           name="message"
           placeholder="Click to begin typing"
@@ -37,6 +41,9 @@ function SendMail() {
           className="sendMail__message"
           ref={register({ required: true })}
         />
+        {errors.message && (
+          <p className="sendMail__error">"Message" is required</p>
+        )}
         <div className="sendMail__options">
           <Button className="sendMail__send">Send</Button>
         </div>
