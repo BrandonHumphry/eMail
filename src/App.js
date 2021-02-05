aa76ee12a3182ab33e6676894da3594028e5d527
Brandon Humphry <brandonhumphry@gmail.com>
2021-02-04 23:22:12 -0800

    created a userSlice to handle the state for user authentication(in progress)

--- src/App.js
+++ src/App.js
@@ -8,27 +8,35 @@ import EmailList from "./EmailList";
 import SendMail from "./SendMail";
 import { useSelector } from "react-redux";
 import { selectSendMessageIsOpen } from "./features/mailSlice";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
+
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+  const user = useSelector(selectUser);
   return (
     <Router>
-      <div className="app">
-        <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
 
-        <div className="app__body">
-          <Sidebar />
-          {/* switch mail content */}
-          <Switch>
-            <Route path="/mail">
-              <Mail />
-            </Route>
-            <Route path="/">
-              <EmailList />
-            </Route>
-          </Switch>
+          <div className="app__body">
+            <Sidebar />
+            {/* switch mail content */}
+            <Switch>
+              <Route path="/mail">
+                <Mail />
+              </Route>
+              <Route path="/">
+                <EmailList />
+              </Route>
+            </Switch>
+          </div>
+          {sendMessageIsOpen && <SendMail />}
         </div>
-        {sendMessageIsOpen && <SendMail />}
-      </div>
+      )}
     </Router>
   );
 }
