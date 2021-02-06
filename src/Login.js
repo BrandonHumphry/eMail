ad994e074aa5893e2024911fdd90da89d945fdbb
Brandon Humphry <brandonhumphry@gmail.com>
2021-02-05 22:59:19 -0800

    styling for login page

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,25 @@
+import { Button } from "@material-ui/core";
 import React from "react";
+import "./Login.css";
+
+const signIn = () => {
+  // auth;
+};
 
 function Login() {
-  return <div></div>;
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://static.dezeen.com/uploads/2020/10/gmail-google-logo-rebrand-workspace-design_dezeen_2364_col_0.jpg"
+          alt=""
+        />
+        <Button variant="contained" color="primary" onClick={signIn}>
+          Log In
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
